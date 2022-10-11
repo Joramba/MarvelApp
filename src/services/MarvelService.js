3b3c472c19f68915ee543c8b835068ae6eddf858
Joramba <jorkabozko@gmail.com>
2022-10-11 22:13:35 +0200

    feat: add card selection

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,8 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=b10c59979fb4c72897ade3b42ced8aec';
+    _baseOffset = 210;
+
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -12,8 +14,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=200&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
